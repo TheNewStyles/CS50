bcb74077e14101dfd9c4f999ef5d75d1ed54a4d2
TheNewStyles <tyler.peterson@utah.edu>
2018-04-11 16:16:11 -0600

    Add solution for porting c to python

---
+++ mario.py
@@ -0,0 +1,32 @@
+import cs50, sys
+
+def main():
+    height = get_arg()
+    draw_pyramids(height)
+    
+def get_arg():
+    if len(sys.argv) != 2 or int(sys.argv[1]) > 23 or int(sys.argv[1]) < 0:
+        return;
+    return int(sys.argv[1])
+    
+def draw_pyramids(pyH):
+    for i in range(pyH):
+        draw_left(pyH, i)
+        print("  ", end="", flush=True)
+        draw_right(i)
+        print("\n")
+        
+def draw_left(pyH, i):
+    for j in range(pyH-i):
+        print(" ", end="", flush=True)
+    
+    for i in range(i):
+        print("#", end="", flush=True)
+    
+def draw_right(i):
+    for i in range(i):
+        print("#", end="", flush=True)
+        True
+    
+if __name__ == "__main__":
+    main()
